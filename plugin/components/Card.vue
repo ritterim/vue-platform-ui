d40c86372077b311802b3c1f0e0cdc1de387eb0f
Andrew Rady <andrew.rady@ritterim.com>
2021-01-19 10:37:29 -0700

    adds basic card and button component structure with use in 'playground'

---
+++ plugin/components/Card.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card">
+    <div class="card__header">
+      <slot name="header">
+      </slot>
+    </div>
+    <slot name="content"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card'
+}
+</script>
