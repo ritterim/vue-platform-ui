d0e535f1f6f30205e8ca7f0e967ae42d64b677d7
Andrew Rady <andrew.rady@ritterim.com>
2021-01-18 22:15:58 -0700

    moved plugin and components into different directory

--- src/components/Accordion.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue';
-import Accordion from './Accordion.vue';
-
-const app = createApp(Accordion);
-
-app.mount('#app');
--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import PuiPlugin from './plugin.js';
+import PuiPlugin from '../plugin/plugin.js';
 
 const app = createApp(App);
 
