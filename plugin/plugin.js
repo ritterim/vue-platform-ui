f8c4c21ae5eb64b4899e45d97342b4dd158bf7de
Austin Asbury <austin.asbury@ritterim.com>
2021-01-20 09:18:01 -0500

    testing components with new file structure

--- plugin/plugin.js
+++ plugin/plugin.js
@@ -1,11 +1,15 @@
 import Accordion from './components/Accordion.vue';
 import Card from './components/Card.vue';
 import Button from './components/Button.vue';
+import Menu from './components/Menu.vue';
+import MenuDropdown from './components/MenuDropdown.vue';
 
 export default {
   install: (app) => {
     app.component('pui-accordion', Accordion);
     app.component('pui-card', Card);
     app.component('pui-button', Button);
+    app.component('pui-menu', Menu);
+    app.component('pui-menu-dropdown', MenuDropdown);
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-import './components/Accordion.js';
-import '../node_modules/@ritterim/platform-ui/dist/platform-ui.min.css';
-import './components/Menu.js'
-import './components/MenuDropdown.js'
-=======
 import { createApp } from 'vue';
 import App from './App.vue';
 import PuiPlugin from '../plugin/plugin.js';
@@ -12,4 +6,3 @@ const app = createApp(App);
 
 app.use(PuiPlugin);
 app.mount('#app');
->>>>>>> master
