e5dc14ef0d5e9471076a33a2b967eaa2122f05ac
Austin Asbury <austin.asbury@ritterim.com>
2021-01-22 10:32:50 -0500

    creating table component

---
+++ plugin/components/Table.vue
@@ -0,0 +1,25 @@
+<template>
+  <table class="table">
+    <thead>
+      <tr>
+        <th v-repeart="headers">
+          {}
+        </th>
+      </tr>
+    </thead>
+  </table>
+</template>
+
+<script>
+export default {
+  name: 'pui-table',
+  props: {
+    unresponsive: {
+      type: Boolean
+    },
+    headers: {
+      type: Array
+    }
+  }
+};
+</script>
