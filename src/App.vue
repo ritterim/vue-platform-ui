c9848b244d6502d3fb2425c3d2e87958a0ad1ff2
Ryan Trimble <mrtrimble2@gmail.com>
2021-01-25 13:09:13 -0500

    updated to include alt attr

--- src/App.vue
+++ src/App.vue
@@ -216,15 +216,18 @@
         <h2 class="mt-6">Image Round</h2>
         <pui-image-round
           src="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
-          size="sm">
+          size="sm"
+          alt="Purple mountains">
         </pui-image-round>
         <pui-image-round
           src="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
-          size="md">
+          size="md"
+          alt="Purple mountains">
         </pui-image-round>
         <pui-image-round
           src="https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
-          size="lg">
+          size="lg"
+          alt="Purple mountains">
         </pui-image-round>
       </div>
     </div>
